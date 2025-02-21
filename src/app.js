4e643703d659207d420d943d8856f87aab0d8317
Jhon Cordova <jhon.a.cordova.martinez@gmail.com>
2025-02-21 09:57:16 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,19 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let quien = ['el perro', 'mi abuela', 'el cartero', 'mi ave']
+  let accion = ['comio', 'destruyo', 'rompio', 'escondio']
+  let que = ['mi tarea', 'mi trabajo', 'mi carro']
+  let cuando = ['durante la clase', 'mientras dormia', 'durante el almuerzo', 'mientras rezaba', 'mientras entrenaba' ]
+
+ let quienRandom = [Math.floor(Math.random() * quien.length)];
+ let accionRandom = [Math.floor(Math.random() * accion.length)];
+ let queRandom = [Math.floor(Math.random() * que.length)];
+ let cuandoRandom = [Math.floor(Math.random() * cuando.length)];
+
+ let excusa = `${quien[quienRandom]} ${accion[accionRandom]} ${que[queRandom]} ${cuando[cuandoRandom]}`;
+
+  console.log(excusa);
+  document.querySelector("#excuse").innerHTML = excusa
 };
